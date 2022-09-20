8b3a37b86aca57e3f7bd9aa93946558f6154ca4a
Diana Müller <diana.muller@globalhitss.com.br>
2022-09-20 11:12:12 -0300

    start work

--- pomodoro/src/pages/Default/modules/usePomodoroDefaultTimer.js
+++ pomodoro/src/pages/Default/modules/usePomodoroDefaultTimer.js
@@ -1,46 +1,69 @@
 import { useEffect, useState } from "react";
 
 export const usePomodoroDefaultTimer = () => {
-  const [defaultTime, setDefaultTime] = useState({
-    workTime: 1,
-    shortTime: 5,
-    longTime: 15,
-    sessions:4
-  });
+  const [defaultTime, setDefaultTime] = useState([
+    {workTime: 1, message:'Foque'},
+    {shortTime: 5, message: 'Descanse'},
+    {longTime: 15, message: 'Descanse'}
+  ]);
+  const [sessions, setSessions] = useState(4);
+  const [isCounting, setIsCounting] = useState(false)
 
-  const [isActive, setIsActive] = useState(false);
+  const [stepPomodoro, setStepPomodoro] = useState({
+    work: false,
+    shortBreak:false,
+    longBreak: false,
+    end: false
+  });
+ 
+  // const [isPaused, setIsPaused] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
-  const [currentTime, setCurrentTime] = useState(defaultTime.workTime*60);
+
+  const [currentTime, setCurrentTime] = useState(defaultTime[0].workTime * 60);
 
   const minutes = Math.floor(currentTime / 60);
   const seconds = Math.floor(currentTime % 60);
 
   function StartPomodoro() {
-    console.log("click");
-    setIsActive(true);
-    setCurrentTime(defaultTime.workTime * 60);
-    // const timer = setInterval(() => {
-    //   setCurrentTime((prevDefaultTime) => prevDefaultTime - 1);
-    //   if(minutes === 0 && seconds ===0){
-    //     console.log("zerou")
-    //     clearInterval(timer);
-    //   }
-    // }, 1000);
-
+    console.log("start");
+    WorkPomodoro();
+  }
+  
+  function WorkPomodoro() {
+    console.log("workPomodoro");
+    setIsCounting(true);
   }
 
+
+  // function BreakPomodoro() {
+  //   console.log("intervalo");
+  //   setCurrentTime(defaultTime.workTime * 60);
+  //   if(sessions>1 ? setCurrentTime(defaultTime.shortTime * 60) : setCurrentTime(defaultTime.longTime * 60));
+  // }
+
   useEffect(()=>{
-    if(currentTime > 0 && isActive){
-      console.log("workTimeFinished")
-      let countdownTimeOut = setTimeout(()=>{
-        setCurrentTime(currentTime -1);
-      }, 100)
-    } else if(isActive && currentTime === 0){
-      console.log('finished')
-      setHasFinished(true);
-      setIsActive(false);
+    let interval = setInterval(() => {
+      isCounting && setCurrentTime((currentTime) => (currentTime >=1 ? currentTime -1 : 0))
+    }, 100);
+
+    return () => {
+      clearInterval(interval)
     }
-  }, [currentTime, isActive]);
+    // if(currentTime > 0 && isCounting){
+    //   let countdownTimeOut = setInterval(()=>{
+    //     setCurrentTime(currentTime -1);
+    //   }, 100)
+    // } 
+    // else if(isCounting && currentTime === 0){
+    //   console.log('finished')
+      
+    //   setIsCounting(false);
+    //   BreakPomodoro();
+    // } 
+    // else if(isCounting && currentTime === 0){
+
+    // }
+  }, [isCounting]);
 
   return {
     StartPomodoro,
