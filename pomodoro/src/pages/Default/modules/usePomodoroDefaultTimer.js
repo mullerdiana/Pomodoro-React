73702faab610b930e30f4a7fda3590f598ff08d1
Diana Müller <diana.muller@globalhitss.com.br>
2022-09-19 22:24:22 -0300

    useEffect

--- pomodoro/src/pages/Default/modules/usePomodoroDefaultTimer.js
+++ pomodoro/src/pages/Default/modules/usePomodoroDefaultTimer.js
@@ -1,62 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const usePomodoroDefaultTimer = () => {
   const [defaultTime, setDefaultTime] = useState({
-    workTime: 25,
+    workTime: 1,
     shortTime: 5,
     longTime: 15,
+    sessions:4
   });
 
-  const [currentTime, setCurrentTime] = useState(0);
-
-  //   let convertedWorkTime = defaultTime.workTime * 60;
-
-  //   const [remainingTime, setRemainingTime] = useState(convertedWorkTime);
-  //   const [WorkTimer, setWorkTimer] = useState({
-  //     remainingMinutes: Math.trunc(convertedWorkTime / 60),
-  //     remainingSeconds: Math.trunc(convertedWorkTime % 60),
-  //   })
-
-  //   const timerHandler =
-
-  //   function StartPomodoro() {
-  //     console.log('click');
-  //     setRemainingTime = setInterval(() => {
-  //         convertedWorkTime--;
-  //     },100);
-  //   }
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+  const [currentTime, setCurrentTime] = useState(defaultTime.workTime*60);
 
   const minutes = Math.floor(currentTime / 60);
   const seconds = Math.floor(currentTime % 60);
 
   function StartPomodoro() {
     console.log("click");
-
+    setIsActive(true);
     setCurrentTime(defaultTime.workTime * 60);
+    // const timer = setInterval(() => {
+    //   setCurrentTime((prevDefaultTime) => prevDefaultTime - 1);
+    //   if(minutes === 0 && seconds ===0){
+    //     console.log("zerou")
+    //     clearInterval(timer);
+    //   }
+    // }, 1000);
 
-    const timer = setInterval(() => {
-      setCurrentTime((prevDefaultTime) => prevDefaultTime - 1);
-    }, 100);
   }
 
-  console.log(defaultTime, defaultTime.workTime);
+  useEffect(()=>{
+    if(currentTime > 0 && isActive){
+      console.log("workTimeFinished")
+      let countdownTimeOut = setTimeout(()=>{
+        setCurrentTime(currentTime -1);
+      }, 100)
+    } else if(isActive && currentTime === 0){
+      console.log('finished')
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [currentTime, isActive]);
 
   return {
     StartPomodoro,
     minutes,
     seconds
-    // defaultTime,
-    // remainingTime,
-    // setRemainingTime,
-    // WorkTimer
   };
 };
 
-//conversão para para o setInterval atualizar minutos e seg restantes
-// workTime *60
-// shortTime *60,
-// longTime *60
-// }
-
-// minutes = valor convertido / 60
-// seconds = valor convertido % 60
